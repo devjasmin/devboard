1c3b191799b454660f162cc1034d6b7bd28d51a8
Jasmin <j.longa@gmx.ch>
2026-08-04 17:05:40 +0200

    Setup React Router und shadcn/ui abgeschlossen.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h3 className="bg-black text-5xl font-bold text-red-500 ml-20 mr-20 mt-10 p-10">
+      <h3 className="bg-black text-xl font-bold text-red-500 p-5">
         Herzlich Willkommen auf dem DevBoard
       </h3>
       <Outlet />
--- src/pages/BoardOverview/BoardOverview.tsx
+++ src/pages/BoardOverview/BoardOverview.tsx
@@ -1,9 +1,14 @@
-function BoardOverview() {
-  console.log("BoardOverview wird geladen");
+import { Button } from "@/components/ui/button";
 
+function BoardOverview() {
   return (
     <>
-      <h2 className="text-blue-600">Hier entsteht die BoardOverview Seite</h2>
+      <div className="flex grid-col gap-30 mt-10 ml-10">
+        <h4>Meine Boards</h4>
+        <Button>Neues Board</Button>
+      </div>
+      <br />
+      <div className="border-amber-500 border-4">Board 3 Spalten 0 Task</div>
     </>
   );
 }
