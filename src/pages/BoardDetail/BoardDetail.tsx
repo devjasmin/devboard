fddfa27128d3ca696d5819f3f8ee749f8e0e7e0c
Jasmin <j.longa@gmx.ch>
2026-08-05 14:23:21 +0200

    Header bei BoardDetail erstellt

--- src/pages/BoardDetail/BoardDetail.tsx
+++ src/pages/BoardDetail/BoardDetail.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { MoveLeftIcon, PencilIcon } from "lucide-react";
+
 function BoardDetail() {
   return (
     <>
-      <h2>Hier entsteht die BoardDetail Seite</h2>
+      <div className="container">
+        <div className="flex flex-row gap-4 mt-5 ml-5">
+          <Button
+            className="hover:text-destructive"
+            size="icon"
+            variant="ghost"
+          >
+            <MoveLeftIcon />
+          </Button>
+          <h1>Neuer Board name</h1>
+          <Button
+            className="hover:text-destructive"
+            size="icon"
+            variant="ghost"
+          >
+            <PencilIcon />
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
