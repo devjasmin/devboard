847c30b5f85264f9402f0a9e7e84cb46f633a4f2
Jasmin <j.longa@gmx.ch>
2026-08-07 13:28:41 +0200

    Neue Sektion Header gebaut und bei App hinzugefügt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { Button } from "./components/ui/button";
+import Header from "./pages/Header";
 import { Outlet } from "react-router-dom";
-import { CircleUser, LayoutDashboard } from "lucide-react";
-import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="flex flex-row place-content-between bg-black ">
-        <Link to="/boards">
-          <h3 className="text-xl font-bold text-red-500 p-5 flex items-center gap-2">
-            <LayoutDashboard /> DevBoard Jasmin
-          </h3>
-        </Link>
-        <Link to="/profile">
-          <Button className="text-white mt-4">
-            <CircleUser /> Jasmin2
-          </Button>
-        </Link>
-      </div>
+      <Header />
       <Outlet />
     </>
   );
---
+++ src/pages/Header.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { CircleUser, LayoutDashboard } from "lucide-react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="flex flex-row place-content-between bg-black ">
+      <Link to="/boards">
+        <h3 className="text-xl font-bold text-red-500 p-5 flex items-center gap-2">
+          <LayoutDashboard /> DevBoard Jasmin
+        </h3>
+      </Link>
+      <Link to="/profile">
+        <Button className="text-white mt-4">
+          <CircleUser /> Jasmin2
+        </Button>
+      </Link>
+    </div>
+  );
+}
+
+export default Header;
